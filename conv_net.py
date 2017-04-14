37f79b37f51519c4fdd6cd392cb9730f9b706f00
Ryan Busby <ryanbusby@Ryans-Macbook.local>
2017-04-14 11:02:53 -0600

    change conv_net n_f/optimizer/dense

--- conv_net.py
+++ conv_net.py
@@ -23,7 +23,7 @@ y = np.load('temp/test_prep_y.npy')
 print (X.shape)
 img_rows, img_cols = X.shape[1], X.shape[2]
 batch_size = 200
-n_f = 64
+n_f = 25
 nb_classes = 8
 nb_epoch = 25
 
@@ -61,22 +61,22 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Convolution2D(n_f*2, 3, 3, border_mode='same'))
+model.add(Convolution2D(n_f/2, 3, 3, border_mode='same'))
 model.add(Activation('relu'))
-model.add(Convolution2D(n_f*2, 3, 3))
+model.add(Convolution2D(n_f/2, 3, 3))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Dropout(.25))
 
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
 model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
+              optimizer='rmsprop',
               metrics=['accuracy'])
 
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
