1008dfddb636f9e62162a26386065f8df0605b5c
Ryan Busby <ryanbusby@Ryans-Macbook.local>
2017-04-14 12:02:14 -0600

    change input shape to 3

--- conv_net.py
+++ conv_net.py
@@ -51,13 +51,13 @@ print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
 if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1,  img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
+    X_train = X_train.reshape(X_train.shape[0], 3,  img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 3, img_rows, img_cols)
+    input_shape = (3, img_rows, img_cols)
 else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 3)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 3)
+    input_shape = (img_rows, img_cols, 3)
 print (input_shape)
 
 Y_train = np_utils.to_categorical(y_train, nb_classes)
